6b245d16fc5d757788feefd64f987eef69877766
tanny <lt14gs@gmail.com>
2025-06-26 10:12:45 -0400

    Fix logo image import path in Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import logoImage from '../assets/logo-jilicharm-fengshui.png';
+import logoImage from '../../assets/logo-jilicharm-fengshui.png';
 
 interface HeaderProps {
   showNewAnalysis?: boolean;
